f5cc0073568dea0fbd86a9ca65b973b320c47663
Rakhshan Zahid <rakhshanzahid@RAKHSHANs-MacBook-Pro.local>
2013-11-17 12:47:35 -0500

    Adding AWS images linkage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@
 
     if Rails.env.production?
 
-      config.fog.credentials = {
+      config.fog_credentials = {
     :provider               => 'AWS',                        
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        
     :aws_secret_access_key  => ENV['AWS_SECRET_KEY']                      
